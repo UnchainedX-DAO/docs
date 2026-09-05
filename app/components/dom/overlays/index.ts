571055251731ae8db5d46653b636b16eb038ba7b
aiinkiestism <g.k.k.r.1228kc@gmail.com>
2026-09-05 22:58:13 +0900

    perf: remove three.js from root chunk via 2D loading screen

--- app/components/dom/overlays/index.ts
+++ app/components/dom/overlays/index.ts
@@ -1,3 +1,2 @@
 export { default as Cursor } from "./Cursor.client";
 export { default as LoadingScreen } from "./LoadingScreen.client";
-export { default as NavigationLoader } from "./NavigationLoader.client";
